6e96e00ac8635df6f8440f61060180f78270ca16
Carlos Jiménez <carlosjimz87@gmail.com>
2021-09-22 18:37:24 +0200

    basic webview

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -1,25 +1,20 @@
 package com.example.webview;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-    private VideoEnabledWebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        WebView webView = new WebView(getBaseContext());
+        setContentView(webView);
+        webView.loadUrl("https://esii-orion.com/orion-display/assets/sounds/esii_call_sounds.mp3");
 
-//        MyWebView webView = new MyWebView(getBaseContext());
-//        setContentView(webView);
-//        webView.loadUrl("https://esii-orion.com/orion-display/assets/sounds/esii_call_sounds.mp3");
-
-//        setContentView(R.layout.activity_main);
-//        AdvancedWebView mWebView = (AdvancedWebView) findViewById(R.id.webview);
-//        mWebView.setMixedContentAllowed(false);
-//        mWebView.loadUrl("https://esii-orion.com/orion-display/assets/sounds/esii_call_sounds.mp3");
     }
 
 }
