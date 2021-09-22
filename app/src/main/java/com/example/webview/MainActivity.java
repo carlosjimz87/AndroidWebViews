301b3d89a6fea6c5705d1ef2b26f52a5a4f522c0
Carlos Jiménez <carlosjimz87@gmail.com>
2021-09-22 18:30:32 +0200

    init (wip)

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.webview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+
+    private VideoEnabledWebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+//        MyWebView webView = new MyWebView(getBaseContext());
+//        setContentView(webView);
+//        webView.loadUrl("https://esii-orion.com/orion-display/assets/sounds/esii_call_sounds.mp3");
+
+//        setContentView(R.layout.activity_main);
+//        AdvancedWebView mWebView = (AdvancedWebView) findViewById(R.id.webview);
+//        mWebView.setMixedContentAllowed(false);
+//        mWebView.loadUrl("https://esii-orion.com/orion-display/assets/sounds/esii_call_sounds.mp3");
+    }
+
+}
